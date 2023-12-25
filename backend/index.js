13c6c7521a859c44af91b27b17abbaa0e77d9790
dheeraj <bangeradheeraj8@gmail.com>
2023-12-26 03:24:26 +0530

    all user routes added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cors = require("cors")
-// const router = require("./routes/loginSignup")
+// const cors = require("cors")
+const userRouter = require("./routes/userRoutes")
 const dbConnect = require("./database/dbConnect")
 const cookieParser = require("cookie-parser")
 require("dotenv").config()
@@ -16,4 +16,4 @@ app.listen(process.env.PORT ||5000,()=>{
  console.log(`server running at ${process.env.PORT ?process.env.PORT :5000}`)
 }) 
 dbConnect();
-//app.use("/v1",router) 
+app.use("/user",userRouter) 
